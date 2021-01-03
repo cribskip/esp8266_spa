5da4c68684edef95bab3397c9c1575708d9e8677
emmanuelmoll <emmanuel.moll@gmail.com>
2021-01-03 16:03:11 +0000

    Hassio autodiscovery

--- src/esp8266_spa.ino
+++ src/esp8266_spa.ino
@@ -1,11 +1,15 @@
 // https://github.com/ccutrer/balboa_worldwide_app/blob/master/doc/protocol.md
-// Reference: https://github.com/ccutrer/balboa_worldwide_app/wiki
+// Reference:https://github.com/ccutrer/balboa_worldwide_app/wiki
 
 // Please install the needed dependencies:
 // CircularBuffer
 // PubSubClient
 
 // TODO:
+// HomeAssistant autodiscover
+// Proper states (rather than just ON/OFF)
+// OTA update from Firebase
+// Settings
 // Implement Existing Client Request/Response
 // Handle non-CTS Client IDs
 
@@ -14,7 +18,7 @@
 // A    YELLOW
 // B    WHITE
 
-#define VERSION "0.1"
+#define VERSION "0.2"
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
 #define BROKER ""
@@ -23,6 +27,9 @@
 
 #define STRON String("ON").c_str()
 #define STROFF String("OFF").c_str()
+
+//HomeAssistant autodiscover
+#define HASSIO true
 
 //#define TX485 D1
 #define RLY1  D7
@@ -61,13 +68,13 @@ uint8_t id = 0x00;
 unsigned long lastrx = 0;
 
 struct {
-  uint8_t jet1 : 1;
-  uint8_t jet2 : 1;
-  uint8_t blower : 1;
-  uint8_t light : 1;
-  uint8_t restmode: 1;
-  uint8_t highrange: 1;
-  uint8_t padding : 2;
+  uint8_t jet1 :1;
+  uint8_t jet2 :1;
+  uint8_t blower :1;
+  uint8_t light :1;
+  uint8_t restmode:1;
+  uint8_t highrange:1;
+  uint8_t padding :2;
 } SpaState;
 
 void _yield() {
@@ -84,7 +91,7 @@ void print_msg(CircularBuffer<uint8_t, 35> &data) {
     s += String(x, HEX);
     s += " ";
   }
-  mqtt.publish("Spa/msg", s.c_str());
+  mqtt.publish("Spa/node/msg", s.c_str());
   _yield();
 }
 
@@ -111,23 +118,52 @@ void reconnect() {
 
 
     if (connection) { //, "Spa/node", 0, true, "OFF")) {
-      mqtt.publish("Spa/node", "ON", true);
-      mqtt.publish("Spa/debug", "RECONNECT");
-      mqtt.publish("Spa/debug", String(millis()).c_str());
-      mqtt.publish("Spa/debug", String(oldstate).c_str());
-      mqtt.publish("Spa/version", VERSION);
-      mqtt.publish("Spa/flashsize", String(ESP.getFlashChipRealSize()).c_str());
-      mqtt.publish("Spa/chipid", String(ESP.getChipId()).c_str());
+
+      // ... Hassio autodiscover
+    if (HASSIO) {
+        //temperature
+        mqtt.publish("homeassistant/sensor/Spa/temperature/config", "{\"name\":\"Hot tub temperature\", \"device_class\":\"temperature\", \"state_topic\":\"Spa/temperature/state\", \"unit_of_measurement\":\"°C\", \"platform\":\"mqtt\"}");
+        //target_temperature
+        //mqtt.publish("homeassistant/switch/Spa/target_temp/config", "{\"name\":\"Hot tub target temperature\", \"command_topic\":\"Spa/target_temp/set\", \"state_topic\":\"Spa/target_temp/state\", \"unit_of_measurement\":\"°C\"}");
+        //heat mode
+        mqtt.publish("homeassistant/switch/Spa/heatingmode/config", "{\"name\":\"Hot tub heating mode\", \"command_topic\":\"Spa/heatingmode/set\", \"state_topic\":\"Spa/heatingmode/state\", \"platform\":\"mqtt\"}");
+        //high range
+        mqtt.publish("homeassistant/switch/Spa/highrange/config", "{\"name\":\"Hot tub high range\", \"command_topic\":\"Spa/highrange/set\", \"state_topic\":\"Spa/highrange/state\", \"platform\":\"mqtt\"}");
+        //circulation pump
+        mqtt.publish("homeassistant/binary_sensor/Spa/circ/config", "{\"name\":\"Hot tub circulation pump\", \"device_class\":\"power\", \"state_topic\":\"Spa/circ/state\", \"platform\":\"mqtt\"}");
+        //heating state
+        mqtt.publish("homeassistant/binary_sensor/Spa/heatstate/config", "{\"name\":\"Hot tub heating state\", \"state_topic\":\"Spa/heatstate/state\", \"platform\":\"mqtt\"}");
+        //light 1
+        mqtt.publish("homeassistant/switch/Spa/light/config", "{\"name\":\"Hot tub light\", \"command_topic\":\"Spa/light/set\", \"state_topic\":\"Spa/light/state\", \"platform\":\"mqtt\"}");
+        //jets 1
+        mqtt.publish("homeassistant/switch/Spa/jet1/config", "{\"name\":\"Hot tub jet1\", \"command_topic\":\"Spa/jet1/set\", \"state_topic\":\"Spa/jet/1/state\", \"platform\":\"mqtt\"}");
+        //jets 2
+        mqtt.publish("homeassistant/switch/Spa/jet2/config", "{\"name\":\"Hot tub jet2\", \"command_topic\":\"Spa/jet2/set\", \"state_topic\":\"Spa/jet/2/state\", \"platform\":\"mqtt\"}");
+        //blower
+        mqtt.publish("homeassistant/switch/Spa/blower/config", "{\"name\":\"Hot tub blower\", \"command_topic\":\"Spa/blower/set\", \"state_topic\":\"Spa/blower/state\", \"platform\":\"mqtt\"}");
+
+    }
+
+
+      mqtt.publish("Spa/node/state", "ON", true);
+      mqtt.publish("Spa/node/debug", "RECONNECT");
+      mqtt.publish("Spa/node/debug", String(millis()).c_str());
+      mqtt.publish("Spa/node/debug", String(oldstate).c_str());
+      mqtt.publish("Spa/node/version", VERSION);
+      mqtt.publish("Spa/node/flashsize", String(ESP.getFlashChipRealSize()).c_str());
+      mqtt.publish("Spa/node/chipid", String(ESP.getChipId()).c_str());
 
       // ... and resubscribe
       mqtt.subscribe("Spa/command");
-      mqtt.subscribe("Spa/temperature");
-      mqtt.subscribe("Spa/heatmode");
-      mqtt.subscribe("Spa/highrange");
-      mqtt.subscribe("Spa/jets/#");
-      mqtt.subscribe("Spa/blower");
-      mqtt.subscribe("Spa/light");
-      mqtt.subscribe("Spa/relay/#");
+      mqtt.subscribe("Spa/target_temp/set");
+      mqtt.subscribe("Spa/heatmode/set");
+      mqtt.subscribe("Spa/highrange/set");
+      mqtt.subscribe("Spa/jets/1/set");
+      mqtt.subscribe("Spa/jets/2/set");
+      mqtt.subscribe("Spa/blower/set");
+      mqtt.subscribe("Spa/light/set");
+      mqtt.subscribe("Spa/relay/1/set");
+      mqtt.subscribe("Spa/relay/2/set");
 
       last_state_crc = 0x00;
     }
@@ -143,7 +179,7 @@ void callback(char* p_topic, byte * p_payload, unsigned int p_length) {
   }
   String topic = String(p_topic);
 
-  mqtt.publish("Spa/debug", topic.c_str());
+  mqtt.publish("Spa/node/debug", topic.c_str());
   _yield();
 
   // handle message topic
@@ -167,28 +203,25 @@ void callback(char* p_topic, byte * p_payload, unsigned int p_length) {
     }
   } else if (topic.equals("Spa/command")) {
     if (payload.equals("reset")) hardreset();
-  } else if (topic.equals("Spa/heatmode")) {
+  } else if (topic.equals("Spa/heatingmode/set")) {
     if (payload.equals("ON") && SpaState.restmode == 1) send = 0x51; // ON = Ready; OFF = Rest
     else if (payload.equals("OFF") && SpaState.restmode == 0) send = 0x51;
-  } else if (topic.equals("Spa/light")) {
+  } else if (topic.equals("Spa/light/set")) {
     if (payload.equals("ON") && SpaState.light == 0) send = 0x11;
     else if (payload.equals("OFF") && SpaState.light == 1) send = 0x11;
-  } else if (topic.startsWith("Spa/jets/")) {
-    if (topic.charAt(9) == '1')  {
-      if (payload.equals("ON") && SpaState.jet1 == 0) send = 0x04;
-      else if (payload.equals("OFF") && SpaState.jet1 == 1) send = 0x04;
-    }
-    else if (topic.charAt(9) == '2') {
-      if (payload.equals("ON") && SpaState.jet2 == 0) send = 0x05;
-      else if (payload.equals("OFF") && SpaState.jet2 == 1) send = 0x05;
-    }
-  } else if (topic.equals("Spa/blower")) {
+  } else if (topic.equals("Spa/jet/1/set")) {
+    if (payload.equals("ON") && SpaState.jet1 == 0) send = 0x04;
+    else if (payload.equals("OFF") && SpaState.jet1 == 1) send = 0x04;
+  } else if (topic.equals("Spa/jet/2/set")) {
+    if (payload.equals("ON") && SpaState.jet2 == 0) send = 0x05;
+    else if (payload.equals("OFF") && SpaState.jet2 == 1) send = 0x05;
+  } else if (topic.equals("Spa/blower/set")) {
     if (payload.equals("ON") && SpaState.blower == 0) send = 0x0C;
     else if (payload.equals("OFF") && SpaState.blower == 1) send = 0x0C;
-  } else if (topic.equals("Spa/highrange")) {
+  } else if (topic.equals("Spa/highrange/set")) {
     if (payload.equals("ON") && SpaState.highrange == 0) send = 0x50; //ON = High, OFF = Low
     else if (payload.equals("OFF") && SpaState.highrange == 1) send = 0x50;
-  } else if (topic.equals("Spa/temperature")) {
+  } else if (topic.equals("Spa/target_temp/set")) {
     // Get new set temperature
     double d = payload.toDouble();
     if (d > 0) d *= 2; // Convert to internal representation
@@ -263,7 +296,7 @@ void loop() {
     lastrx = millis();
   }
 
-  // DEBUG: mqtt.publish("Spa/rcv", String(x).c_str()); _yield();
+  // DEBUG:mqtt.publish("Spa/rcv", String(x).c_str()); _yield();
 
   // Double SOF-marker, drop last one
   if (Q_in[1] == 0x7E && Q_in.size() > 1) Q_in.pop();
@@ -277,22 +310,22 @@ void loop() {
     if (id == 0) {
       if (Q_in[2] == 0xFE) print_msg(Q_in);
 
-      // FE BF 02: got new client ID
+      // FE BF 02:got new client ID
       if (Q_in[2] == 0xFE && Q_in[4] == 0x02) {
         id = Q_in[5];
         if (id > 0x2F) id = 0x2F;
 
         ID_ack();
-        mqtt.publish("Spa/id", String(id).c_str());
+        mqtt.publish("Spa/node/id", String(id).c_str());
       }
 
-      // FE BF 00: Any new clients?
+      // FE BF 00:Any new clients?
       if (Q_in[2] == 0xFE && Q_in[4] == 0x00) {
         ID_request();
       }
     } else if (Q_in[2] == id) {
       if (Q_in[4] == 0x06) {
-        // id BF 06: Ready to Send
+        // id BF 06:Ready to Send
         if (send == 0xff) {
           // 0xff marks dirty temperature for now
           Q_out.push(id);
@@ -319,7 +352,7 @@ void loop() {
     }
 
     // Normal package handling
-    // FF AF 13: Status Update - Packet index offset 5
+    // FF AF 13:Status Update - Packet index offset 5
     if (Q_in[2] == 0xFF && Q_in[4] == 0x13) {
       if (last_state_crc != Q_in[Q_in[1]]) {
         String s;
@@ -329,12 +362,12 @@ void loop() {
         // DEBUG for finding meaning:
         //print_msg(Q_in);
 
-        // 25: Flag Byte 20 - Set Temperature
+        // 25:Flag Byte 20 - Set Temperature
         d = Q_in[25] / 2;
         if (Q_in[25] % 2 == 1) d += 0.5;
-        mqtt.publish("Spa/state/target", String(d, 2).c_str());
+        mqtt.publish("Spa/target_temp/state", String(d, 2).c_str());
 
-        // 7: Flag Byte 2 - Actual temperature
+        // 7:Flag Byte 2 - Actual temperature
         if (Q_in[7] != 0xFF) {
           d = Q_in[7] / 2;
           if (Q_in[7] % 2 == 1) d += 0.5;
@@ -342,81 +375,81 @@ void loop() {
             if ((d > c * 1.2) || (d < c * 0.8)) d = c; //remove spurious readings greater or less than 20% away from previous read
           }
 
-          mqtt.publish("Spa/state/temperature", String(d, 2).c_str());
+          mqtt.publish("Spa/temperature/state", String(d, 2).c_str());
           c = d;
         } else {
           d = 0;
         }
         // REMARK Move upper publish to HERE to get 0 for unknown temperature
 
-        // 8: Flag Byte 3 Hour & 9: Flag Byte 4 Minute => Time
+        // 8:Flag Byte 3 Hour & 9:Flag Byte 4 Minute => Time
         if (Q_in[8] < 10) s = "0"; else s = "";
         s = String(Q_in[8]) + ":";
         if (Q_in[9] < 10) s += "0";
         s += String(Q_in[9]);
-        mqtt.publish("Spa/state/time", s.c_str());
+        mqtt.publish("Spa/time/state", s.c_str());
 
-        // 10: Flag Byte 5 - Heating Mode
+        // 10:Flag Byte 5 - Heating Mode
         switch (Q_in[10]) {
-          case 0: mqtt.publish("Spa/state/heatingmode", STRON); //Ready
-          case 3: // Ready-in-Rest
+          case 0:mqtt.publish("Spa/heatingmode/state", STRON); //Ready
+          case 3:// Ready-in-Rest
             SpaState.restmode = 0;
             break;
-          case 1: mqtt.publish("Spa/state/heatingmode", STROFF); //Rest
+          case 1:mqtt.publish("Spa/heatingmode/state", STROFF); //Rest
             SpaState.restmode = 1;
             break;
         }
 
-        // 15: Flags Byte 10 / Heat status, Temp Range
+        // 15:Flags Byte 10 / Heat status, Temp Range
         d = bitRead(Q_in[15], 4);
-        if (d == 0) mqtt.publish("Spa/state/heatstate", STROFF);
-        else if (d == 1 || d == 2) mqtt.publish("Spa/state/heatstate", STRON);
+        if (d == 0) mqtt.publish("Spa/heatstate/state", STROFF);
+        else if (d == 1 || d == 2) mqtt.publish("Spa/heatstate/state", STRON);
 
         d = bitRead(Q_in[15], 2);
         if (d == 0) {
-          mqtt.publish("Spa/state/highrange", STROFF); //LOW
+          mqtt.publish("Spa/highrange/state", STROFF); //LOW
           SpaState.highrange = 0;
         } else if (d == 1) {
-          mqtt.publish("Spa/state/highrange", STRON); //HIGH
+          mqtt.publish("Spa/highrange/state", STRON); //HIGH
           SpaState.highrange = 1;
         }
 
-        // 16: Flags Byte 11
+        // 16:Flags Byte 11
         if (bitRead(Q_in[16], 1) == 1) {
-          mqtt.publish("Spa/state/jets1", STRON);
+          mqtt.publish("Spa/jet/1/state", STRON);
           SpaState.jet1 = 1;
         } else {
-          mqtt.publish("Spa/state/jets1", STROFF);
+          mqtt.publish("Spa/jet/1/state", STROFF);
           SpaState.jet1 = 0;
         }
 
         if (bitRead(Q_in[16], 3) == 1) {
-          mqtt.publish("Spa/state/jets2", STRON);
+          mqtt.publish("Spa/jet/2/state", STRON);
           SpaState.jet2 = 1;
         } else {
-          mqtt.publish("Spa/state/jets2", STROFF);
+          mqtt.publish("Spa/jet/2/state", STROFF);
           SpaState.jet2 = 0;
         }
 
-        // 18: Flags Byte 13
+        // 18:Flags Byte 13
         if (bitRead(Q_in[18], 1) == 1)
-          mqtt.publish("Spa/state/circ", STRON);
+          mqtt.publish("Spa/circ/state", STRON);
         else
-          mqtt.publish("Spa/state/circ", STROFF);
+          mqtt.publish("Spa/circ/state", STROFF);
 
         if (bitRead(Q_in[18], 2) == 1) {
-          mqtt.publish("Spa/state/blower", STRON);
+          mqtt.publish("Spa/blower/state", STRON);
           SpaState.blower = 1;
         } else {
-          mqtt.publish("Spa/state/blower", STROFF);
+          mqtt.publish("Spa/blower/state", STROFF);
           SpaState.blower = 0;
         }
-        // 19: Flags Byte 14
+        // 19:Flags Byte 14
         if (Q_in[19] == 0x03) {
-          mqtt.publish("Spa/state/light", STRON);
+          mqtt.publish("Spa/light/state", STRON);
           SpaState.light = 1;
         } else {
-          mqtt.publish("Spa/state/light", STROFF);
+          mqtt.publish("Spa/light/state", STROFF);
           SpaState.light = 0;
         }
 
